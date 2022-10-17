7227107bd36fefb36da5f20006e21dc13e300c8f
carlduy2002 <duydhgcc200018@fpt.edu.vn>
2022-10-17 19:25:50 +0700

    update

--- connection.php
+++ connection.php
@@ -9,7 +9,7 @@
 // $db = "Toy_Web";
 // $conn = pg_connect($server, $username, $password, $db);
 $conn = pg_connect("postgres://izslbhgirodqpi:f35b00a3d5896ade13e27f780d341af50e971929370a023c0f1a5564ad04761a@ec2-52-4-87-74.compute-1.amazonaws.com:5432/d1s4jmvdpoppn1");
-if(!$Connect){
+if(!$conn){
     die("Connection failed");
 }
 ?>
