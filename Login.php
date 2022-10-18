0edf9650784daf69481a25b39c9777abd5c8061a
carlduy2002 <duydhgcc200018@fpt.edu.vn>
2022-10-18 08:32:33 +0700

    updatetoday

--- Login.php
+++ Login.php
@@ -11,7 +11,7 @@
         $sql = "Select type from account where username='$uname' and password='$pwd'";
         $qr = pg_query($conn, $sql);
         // $r = pg_fetch_row($qr);
-        $r = pg_fetch_assoc($qr);
+        $r = pg_fetch_array($qr, PGSQL_ASSOC);
 
         if(pg_num_rows($qr) > 0){
             if($r['type'] == 'Admin'){//$r[6]
